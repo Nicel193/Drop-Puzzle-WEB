d1083f58d55b3fb713301c4e24acd023d2ed2e3a
admin <nicelgoog@gmail.com>
2024-06-12 23:35:31 +0300

    Add scene load callback

--- unity.js
+++ unity.js
@@ -1,20 +1,21 @@
+var _unityInstance;
+
 function getUserIdFromUrl() {
     const params = new URLSearchParams(window.location.search);
     return params.get('userId');
 }
 
-function sendUserIdToUnity(unityInstance) {
-    const userId = getUserIdFromUrl();
-    if (userId) {
-        // console.log('User ID:', userId);
+function onUnitySceneLoaded() {
+    window.alert("Hello, world!");
 
-        setTimeout(function() {
-            if (typeof unityInstance !== 'undefined') {
-                unityInstance.SendMessage('UserIdReceiver', 'ReceiveUserId', userId);
-            } else {
-                console.error('Unity instance is not defined');
-            }
-        }, 5000);
+    const userId = getUserIdFromUrl();
+
+    if (userId) {
+        if (typeof _unityInstance !== 'undefined') {
+            _unityInstance.SendMessage('UserIdReceiver', 'ReceiveUserId', userId);
+        } else {
+            console.error('Unity instance is not defined');
+        }
     } else {
         console.log('User ID не найден');
     }
