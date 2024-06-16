470cfd17bc6392188cc13bb2577be8d456e223c2
admin <nicelgoog@gmail.com>
2024-06-17 01:28:51 +0300

    Return old get user id

--- unity.js
+++ unity.js
@@ -1,10 +1,13 @@
 var _unityInstance;
-var tg = window.Telegram.WebApp;
-var userId = tg.initDataUnsafe.user.id;
 
-tg.expand();
+function getUserIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('userId');
+}
 
 function onUnitySceneLoaded() {
+    const userId = getUserIdFromUrl();
+
     if (userId) {
         if (typeof _unityInstance !== 'undefined') {
             _unityInstance.SendMessage('UserIdReceiver', 'ReceiveUserId', userId);
