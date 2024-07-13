4122d0057f287f78f74d7be1513198b7d1e7bb8c
admin <nicelgoog@gmail.com>
2024-07-13 22:34:04 +0300

    Remove telegram alert

--- unity.js
+++ unity.js
@@ -1,13 +1,13 @@
 var _unityInstance;
 var tg = Telegram.WebApp;
 
-function showTelegramAlert(message) {
-    if (tg) {
-        tg.showAlert(message);
-    } else {
-        alert(message);
-    }
-}
+// function showTelegramAlert(message) {
+//     if (tg) {
+//         tg.showAlert(message);
+//     } else {
+//         alert(message);
+//     }
+// }
 
 showTelegramAlert(`UserId: ${tg.initDataUnsafe.user.id}, FirstName: ${tg.initDataUnsafe.user.first_name}, Test get user id`);
 
